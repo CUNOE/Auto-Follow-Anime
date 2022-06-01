a648afe14c702a95914785eece7a0c3583a70839
Cunoe <cunkycheng@gmail.com>
2022-06-01 12:03:15 +0800

    init

--- enter.sh
+++ enter.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 apt-get update
 apt-get install cron -y
+apt-get -qq install -y --no-install-recommends ca-certificates curl
 sed -i 's/required/sufficient/' /etc/pam.d/cron
 echo "0 */1 * * * root /root/app" >> /etc/crontab
 /etc/init.d/cron restart
